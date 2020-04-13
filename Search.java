fe2d7290acd237c2c870ed3cae1dcaed93abb78c
Pierce Avner <jpavner@gmail.com>
2020-04-12 20:50:04 -0600

    finished binarySearch

--- Search.java
+++ Search.java
@@ -3,8 +3,8 @@ public class Search{
     public static boolean debugging = false;
     public static void main(String[] args){
         ArrayList<String> haystack = new ArrayList<String>();
-        int needleIndex = 9;
-        for(int i = 0; i < 15; i++){
+        int needleIndex = 27;
+        for(int i = 0; i <= 27; i++){
             if(i == needleIndex){
                 haystack.add("needle");
             } else {
@@ -20,6 +20,8 @@ public class Search{
         if(debugging){
             System.out.println(search(haystack,"needle"));
         }
+
+        System.out.println(binarySearch(haystack, "needle"));
     }
 
     public static int search(ArrayList<String> haystack, String needle){
@@ -33,6 +35,25 @@ public class Search{
                 return i;
             }
             i++;
+        }
+        return -1;
+    }
+
+    public static int binarySearch(ArrayList<String> haystack, String needle){
+        boolean searching = true;
+        int half = haystack.size() / 2;
+        for(int i = 0; i < 10; i ++){
+            if(debugging){
+                System.out.println(half);
+            }
+            if(haystack.get(half).compareTo(needle) == 0){
+                searching = false;
+                return half ;
+            } else if(haystack.get(half).compareTo(needle) < 0){
+                half = (half / 2) + (haystack.size() / 2);
+            } else {
+                half = half / 2;
+            }
         }
         return -1;
     }
